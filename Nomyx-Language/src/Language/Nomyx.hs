0927822ac20095bc46f9effe52d3ca9b5decb631
Corentin <cdupont@cdupont-Latitude-E6410.(none)>
2013-04-29 10:35:04 +0200

    re-exports

---
+++ Nomyx-Language/src/Language/Nomyx.hs
@@ -0,0 +1,16 @@
+
+module Language.Nomyx (
+   module Language.Nomyx.Expression,
+   module Language.Nomyx.Definition,
+   module Language.Nomyx.Rule,
+   module Language.Nomyx.Examples,
+   module Language.Nomyx.Evaluation,
+   module Language.Nomyx.Rule)  where
+
+import Language.Nomyx.Expression
+import Language.Nomyx.Definition
+import Language.Nomyx.Rule
+import Language.Nomyx.Examples
+import Language.Nomyx.Evaluation
+import Language.Nomyx.Rule
+
